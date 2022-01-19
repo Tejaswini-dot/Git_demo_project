2686123986b3efdb05e4c109f638b32a45cb8bbc
ArunKumar HG <ArunKumar HG@DESKTOP-75MHIE1>
2022-01-19 19:47:17 +0530

    commiting test2 to branchx

---
+++ src/defaultPackage/Test2.java
@@ -0,0 +1,16 @@
+package defaultPackage;
+
+public class Test2 {
+	public static void main(String[] args)
+	{
+	int n=1500;
+		for (int i=n;i<=2700;i++)
+		{
+		if((i%7==0)&&(i%5==0))
+		{
+			System.out.println( " " + i);
+		}
+		
+	}
+	}
+}
